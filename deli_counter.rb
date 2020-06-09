a705dd1cc28b883d531d7949fd44d69df1b0beae
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-09 04:50:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(line_deli)
   if line_deli.size == 0
      puts "The line is currently empty."
   else 
-       empty_line = line_deli.map.with_index(1) {|customer, number| "#{number}. #{customer}"}
+     empty_line = line_deli.map.with_index(1) {|customer, number| "#{number}. #{customer}"}
      puts "The line is currently: #{empty_line.join(' ')}"
   end
 end
@@ -15,7 +15,7 @@ end
 def now_serving(line_deli)
   if line_deli.empty?
     puts "There is nobody waiting to be served!"
-    else
+  else
     puts "Currently serving #{line_deli.shift}."
   end
 end
